3e28d12754ff1accd7b80c50718b16b962fcfa56
Ghost <57673426+ghoste0001@users.noreply.github.com>
2025-11-22 04:14:57 +0400

    Add Lua state and script execution support to TextEditor

--- src/ui/TextEditor.h
+++ src/ui/TextEditor.h
@@ -1,15 +1,28 @@
 #pragma once
 
 #include "Gui.h"
+#include "../datatypes/Task.h"
 
 class TextEditor : public Gui {
+public:
     int GetLineCount();
     void OnLog(const std::string& message);
 
     void Draw();
+
+    void SetLuaState(lua_State* s) { L = s; }
+
+    void ExecuteCurrentScript();
+    void StopCurrentScript();
+
 protected:
     std::string text;
     std::string title = "BlockEngine Script Editor";
 
     std::vector<std::string> history;
+
+    lua_State* L = nullptr;
+    bool executedScript = false;
+
+    LuaTask* runningTask = nullptr; 
 };
